177b717169c331f27610b154856dd6329d338e17
HaoNhat <haonguyennhat270225@gmail.com>
2026-01-04 06:31:28 +0700

    Database feat : Tạo procedure sp_ThayDoiMatKhau !

---
+++ DataBase/Pro_sp_ThayDoiMatKhau.sql
@@ -0,0 +1,58 @@
+﻿--create procedure sp_ThayDoiMatKhau
+--(
+--	@ID int,
+--	@NewPassWord varchar(50) -- mật khẩu mới!
+--)
+--as 
+--begin
+--	set nocount on;
+--	-- kiểm tra mật khẩu >= 10 ký tự;
+--	if len(@NewPassWord) < 10
+--	begin
+--	return -1; -- mật khẩu không đủ dài !
+--	end
+	
+--	update [dbo].[TaiKhoan]
+--	set [dbo].[TaiKhoan].PassWord = @NewPassWord
+--	where [dbo].[TaiKhoan].ID = @ID;
+
+--	if @@ROWCOUNT > 0
+--		return 1; -- Cập nhật thành công;
+--	else
+--		return 0; -- Cập nhật thất bại !;
+--end
+
+CREATE OR ALTER PROCEDURE sp_ThayDoiMatKhau
+(
+	@ID int,
+	@NewPassWord varchar(50) -- mật khẩu mới!
+)
+as 
+begin
+	set nocount on;
+	-- kiểm tra mật khẩu >= 10 ký tự;
+	if len(@NewPassWord) < 10 
+	begin
+	return -1; -- Mật khẩu không đủ dài !
+	end
+	if  len(@NewPassWord) > 30
+	begin
+	return -2; -- Mật khẩu quá số lượng  quy định !
+	end
+	
+	update [dbo].[TaiKhoan]
+	set [dbo].[TaiKhoan].PassWord = @NewPassWord
+	where [dbo].[TaiKhoan].ID = @ID;
+
+	if @@ROWCOUNT > 0
+		return 1; -- Cập nhật thành công;
+	else
+		return 0; -- Cập nhật thất bại !;
+end
+
+
+declare @ketQua int;
+exec @ketQua = sp_ThayDoiMatKhau
+	@ID = 1,
+	@NewPassWord = 'aaaaaaaaaaaa';
+SELECT @ketQua AS KetQua;
