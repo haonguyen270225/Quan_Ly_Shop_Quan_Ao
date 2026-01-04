2b54ae81ad7bc602f62a75cdb081d678e7a0e7a6
HaoNhat <haonguyennhat270225@gmail.com>
2026-01-04 19:38:24 +0700

    Data base fix : Hàm SQL dbo.ThongTinTaiKhoan.sql !

--- DataBase/Func_dbo.ThongTinTaiKhoan.sql
+++ DataBase/Func_dbo.ThongTinTaiKhoan.sql
@@ -1,28 +1,32 @@
 
 
-  create function dbo.ThongTinTaiKhoan
-  (
-    @UserName varchar(50),
-    @PassWord varchar(50)
-  )
-  returns table
-  as
-return
-(
-    SELECT TOP (1000) nv.ID
-      ,[MaNhanVien]
-      ,[HoVaTen]
-      ,[SDT]
-      ,[Email]
-      ,[CCCD]
-      ,[DiaChi]
-      ,[ChucVu]
-      ,[GioiTinh]
-      ,[HinhThucLamViec]
-  FROM [Quan_Ly_Shop_Quan_Ao].[dbo].[NhanVien] nv
-  inner join [dbo].[TaiKhoan] tk on tk.IDNhanVien = nv.ID
-  where tk.UserName = 'an.Nguyen' and tk.PassWord = '123456'
-  );
+
+-- Ch?nh s?a
+--DROP FUNCTION dbo.ThongTinTaiKhoan;
+
+--  create function dbo.ThongTinTaiKhoan
+--  (
+--    @UserName varchar(50),
+--    @PassWord varchar(50)
+--  )
+--  returns table
+--  as
+--return
+--(
+--    SELECT TOP (1000) nv.ID
+--      ,[MaNhanVien]
+--      ,[HoVaTen]
+--      ,[SDT]
+--      ,[Email]
+--      ,[CCCD]
+--      ,[DiaChi]
+--      ,[ChucVu]
+--      ,[GioiTinh]
+--      ,[HinhThucLamViec]
+--  FROM [Quan_Ly_Shop_Quan_Ao].[dbo].[NhanVien] nv
+--  inner join [dbo].[TaiKhoan] tk on tk.IDNhanVien = nv.ID
+--  where tk.UserName = @UserName and tk.PassWord = @PassWord
+--  );
 
   -- Cách dùng
-SELECT * FROM dbo.ThongTinTaiKhoan('an.Nguyen', '123456');
+--SELECT * FROM dbo.ThongTinTaiKhoan('binh.tran', '123456');
