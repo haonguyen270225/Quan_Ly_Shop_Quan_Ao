5344f36c6cbc3c95eb463cf36b58dcac572c6131
HaoNhat <haonguyennhat270225@gmail.com>
2026-01-05 13:45:24 +0700

    feat : Trang HoaDon DTO_KhoHang/LoaiSanPham ! 01

---
+++ DTO/KhoHang.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class KhoHang
+    {
+        public int ID { get; set; }
+        public string MaHang {  get; set; }
+
+        public string TenHang {  get; set; }
+
+        public int SoLuong {  get; set; }
+
+        public double Gia { get; set; }
+        public int IDSize { get; set; }
+        public int IDSanPham {  get; set; }
+
+        public KhoHang()
+        {
+            this.ID = -1;
+            this.MaHang = "";
+            this.TenHang = "";
+            this.TenHang = "";
+            this.SoLuong = -1;
+            this.Gia = -1;
+            this.IDSize = -1;
+            this.IDSanPham = -1;
+        }
+    }
+}
---
+++ DTO/LoaiSanPham.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class LoaiSanPham
+    {
+        public int ID { get; set; }
+        public string  MaLoaiSanPham { get; set; }
+        public string TenLoai { get; set; }
+
+        public LoaiSanPham() 
+        {
+            this.ID = -1;
+            this.MaLoaiSanPham= "";
+            this.TenLoai = "";
+        }
+    }
+}
