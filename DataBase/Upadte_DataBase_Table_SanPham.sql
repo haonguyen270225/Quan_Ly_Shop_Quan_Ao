6d97f878b2c6e548d646c137f19dc895cc4ad1a7
HaoNhat <haonguyennhat270225@gmail.com>
2026-01-07 03:19:51 +0700

    DataBase update : Cập nhật Table LoaiSanPham  thêm cột GioiTinh !

---
+++ DataBase/Upadte_DataBase_Table_SanPham.sql
@@ -0,0 +1,6 @@
+﻿alter table [dbo].[LoaiSanPham]
+add GioiTinh tinyint not null default(2) -- nam nữ điều mua được ! 1 - nam , 0 - nữ;
+
+update [dbo].[LoaiSanPham]
+set GioiTinh = 0
+where ID = 6 or ID = 8;
