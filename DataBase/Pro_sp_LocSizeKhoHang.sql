31e4e17ac22f0a15e4f54c29e2478641f871036b
HaoNhat <haonguyennhat270225@gmail.com>
2026-01-07 22:21:03 +0700

    DataBase feat : Tạo store procedure sp_LocSizeKhoHang !

---
+++ DataBase/Pro_sp_LocSizeKhoHang.sql
@@ -0,0 +1,15 @@
+﻿---- Lọc KhoHang theo size ;
+--create procedure sp_LocSizeKhoHang
+--	@MaSize varchar(5) 
+--as
+--begin 
+--	select kh.IDSize , MaHang ,   TenHang , SoLuongTon , Gia , IDSize , IDLoaiSanPham
+--	from [dbo].[KhoHang] as kh
+--	inner join [dbo].[Size] as s
+--	on s.ID = kh.IDSize
+--	where s.MaSize = @MaSize;
+--end
+
+
+-- Cách dùng :
+exec sp_LocSizeKhoHang @MaSize = 'M';
