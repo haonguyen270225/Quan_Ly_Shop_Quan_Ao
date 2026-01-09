eec4edd6bd6ccb28b907154dedeaa4cb98fc599c
HaoNhat <haonguyennhat270225@gmail.com>
2026-01-09 09:36:23 +0700

    update database : Table KhuyenMai !

--- DataBase/Update_DataBase_Add_Table_KhuyenMai.sql
+++ DataBase/Update_DataBase_Add_Table_KhuyenMai.sql
@@ -19,3 +19,26 @@ VALUES
 ('KHAITRUONG', N'Giảm giá 20% nhân dịp khai trương cửa hàng'),
 
 ('KHACHVIP', N'Giảm giá 12% dành cho khách hàng thân thiết');
+
+ALTER TABLE KhuyenMai
+ADD GiaTri INT NOT NULL DEFAULT 0;
+
+UPDATE KhuyenMai
+SET GiaTri = 10
+WHERE MaKhuyenMai = 'TET2026';
+
+UPDATE KhuyenMai
+SET GiaTri = 15
+WHERE MaKhuyenMai = 'SV2026';
+
+UPDATE KhuyenMai
+SET GiaTri = 5
+WHERE MaKhuyenMai = 'TONKHO05';
+
+UPDATE KhuyenMai
+SET GiaTri = 20
+WHERE MaKhuyenMai = 'KHAITRUONG';
+
+UPDATE KhuyenMai
+SET GiaTri = 12
+WHERE MaKhuyenMai = 'KHACHVIP';
