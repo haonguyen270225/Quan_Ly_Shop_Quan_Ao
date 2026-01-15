8af3a1423abc767b2c5a8a01eebad025c852c050
HaoNhat <haonguyennhat270225@gmail.com>
2026-01-15 15:41:58 +0700

    feat : Trang HoaDon.cs 21 !

---
+++ BLL/BLL_LoadingHoaDon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAL;
+namespace BLL
+{
+    public class BLL_LoadingHoaDon
+    {
+       private  DAL_LoadingHoaDon dal_LoadingHoaDon = new DAL_LoadingHoaDon();
+       public List<HoaDon> LoadingHoaDon()
+        {
+            return dal_LoadingHoaDon.LoadingHoaDon();
+        }
+    
+
+    }
+}
---
+++ DAL/DAL_LoadingHoaDon.cs
@@ -0,0 +1,42 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_LoadingHoaDon
+    {
+        public List<HoaDon> LoadingHoaDon()
+        {
+            List<HoaDon> listHoaDon = new List<HoaDon>();
+            SqlConnection conn = DAL_DataAccess.Conn();
+            conn.Open();
+            SqlCommand sqlCommand = new SqlCommand(@"SELECT [ID],[MaHoaDon],[Ngay],[Gio],[TongThu],[IDNhanVien],[IDKhachHang] FROM [Quan_Ly_Shop_Quan_Ao].[dbo].[HoaDon]", conn);
+
+            SqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    HoaDon tmp = new HoaDon();
+                    tmp.ID = Convert.ToInt32(dataReader["ID"]);
+                    tmp.MaHoaDon = dataReader["MaHoaDon"].ToString();
+                    tmp.Ngay = Convert.ToDateTime(dataReader["Ngay"]);
+                    tmp.Gio = (TimeSpan)dataReader["Gio"];
+                    tmp.TongThu = Convert.ToDouble(dataReader["TongThu"]);
+                    tmp.IDNhanVien = Convert.ToInt32(dataReader["IDNhanVien"]);
+                    tmp.IDKhachHang = Convert.ToInt32(dataReader["IDKhachHang"]);
+                    listHoaDon.Add(tmp);
+                }
+            }
+            conn.Close();
+            return listHoaDon;
+        }
+    }
+}
