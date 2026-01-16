040e093c56d78ac865402c63c0124ed9c62291e3
HaoNhat <haonguyennhat270225@gmail.com>
2026-01-16 21:03:29 +0700

    Update : Trang TrangChu.cs 04 !

---
+++ DAL/DAL_KhachHang.cs
@@ -0,0 +1,54 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_KhachHang
+    {
+        public List<KhachHang> LoadingKhachHang()
+        {
+            List<KhachHang> tmp = new List<KhachHang>();
+            SqlConnection conn = DAL_DataAccess.Conn();
+            conn.Open();
+
+            SqlCommand sqlCommand = new SqlCommand(@"SELECT [ID],[MaKhachHang],[HoVaTen],[SDT] FROM [Quan_Ly_Shop_Quan_Ao].[dbo].[KhachHang]", conn);
+
+            SqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    KhachHang khachHang = new KhachHang();
+                    khachHang.ID = Convert.ToInt32(dataReader["ID"]);
+                    khachHang.MaKhachHang = dataReader["MaKhachHang"].ToString();
+                    khachHang.HoVaTen = dataReader["HoVaTen"].ToString();
+                    khachHang.SDT = dataReader["SDT"].ToString();
+                    tmp.Add(khachHang);
+                }
+            }
+            conn.Close();
+            return tmp;
+        }
+
+        public void ThemKhachHang(KhachHang khachHang)
+        {
+            SqlConnection conn = DAL_DataAccess.Conn();
+            conn.Open();
+            SqlCommand sqlCommand = new SqlCommand(@"sp_ThemKhachHang", conn);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@MaKhachHang", khachHang.MaKhachHang);
+            sqlCommand.Parameters.AddWithValue("@HoVaTen", khachHang.HoVaTen);
+            sqlCommand.Parameters.AddWithValue("@SDT", khachHang.SDT);
+            sqlCommand.ExecuteNonQuery();
+            conn.Close();
+        }
+
+    }
+}
---
+++ DAL/DAL_KhuyenMai.cs
@@ -0,0 +1,37 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_KhuyenMai
+    {
+        public List<KhuyenMai> LoadingKhuyenMai()
+        {
+            List<KhuyenMai> listKhuyenMai = new List<KhuyenMai>();
+            SqlConnection conn = DAL_DataAccess.Conn();
+            conn.Open();
+            SqlCommand sqlCommand = new SqlCommand(@"SELECT [ID] , [MaKhuyenMai],[ThongTin] , [GiaTri] FROM [Quan_Ly_Shop_Quan_Ao].[dbo].[KhuyenMai]", conn);
+
+            SqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    KhuyenMai khuyenMai = new KhuyenMai();
+                    khuyenMai.ID = Convert.ToInt32(dataReader["ID"]);
+                    khuyenMai.MaKhuyenMai = dataReader["MaKhuyenMai"].ToString();
+                    khuyenMai.ThongTin = dataReader["ThongTin"].ToString();
+                    listKhuyenMai.Add(khuyenMai);
+                }
+            }
+            conn.Close();
+            return listKhuyenMai;
+        }
+    }
+}
